3bd1587e7fd4b48f11e8db3cecb2ac8316095573
FrosSky <74843737+FrosSky@users.noreply.github.com>
2025-05-13 18:01:47 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.chevstrap.rbx"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 1
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
