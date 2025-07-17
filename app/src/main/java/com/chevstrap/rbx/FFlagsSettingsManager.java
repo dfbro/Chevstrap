b8fa3e781cda6749bf0bfa6568a8ed083c20f72c
dfbro <robloslovers@gmail.com>
2025-07-17 19:15:11 +0700

    Update FFlagsSettingsManager.java

--- app/src/main/java/com/chevstrap/rbx/FFlagsSettingsManager.java
+++ app/src/main/java/com/chevstrap/rbx/FFlagsSettingsManager.java
@@ -11,6 +11,8 @@ import com.chevstrap.rbx.Utility.INeedPath;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
